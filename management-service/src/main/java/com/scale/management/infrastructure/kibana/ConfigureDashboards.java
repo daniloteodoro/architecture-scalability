7f069880bcebf7987342d7b2f3c9082adc523469
Danilo Teodoro <danilo.santost@gmail.com>
2021-01-21 23:20:47 +0100

    Set ES metricbeat index refresh time to 1s and update system.yml file

--- management-service/src/main/java/com/scale/management/infrastructure/kibana/ConfigureDashboards.java
+++ management-service/src/main/java/com/scale/management/infrastructure/kibana/ConfigureDashboards.java
@@ -6,9 +6,9 @@ import kong.unirest.ContentType;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -16,13 +16,15 @@ import java.time.Duration;
 
 @Slf4j
 public class ConfigureDashboards {
+    private static final int DEFAULT_REFRESH_TIME = 1;
 
-    public static void forKibana() {
+    public static void inKibanaAndElasticSearch() {
         new Thread("configure-dashboard") {
             @Override
             public void run() {
                 try {
                     final String kibanaHost = System.getenv().getOrDefault("KIBANA_HOST", "localhost");
+                    final String esHost = System.getenv().getOrDefault("ES_HOST", "localhost");
                     final String configFilenameStr = System.getenv().getOrDefault("INDEX_CONFIG_FILE", "");
                     final InputStream configFile = configFilenameStr.isBlank() ? getLocalDashboardConfiguration() : new FileInputStream(configFilenameStr);
 
@@ -32,7 +34,10 @@ public class ConfigureDashboards {
                             .retryExceptions(Exception.class)
                             .build();
                     Retry retry = Retry.of("kibanaConfigRetry", config);
-                    retry.executeRunnable(() -> applyTemplate(kibanaHost, configFile));
+                    retry.executeRunnable(() -> {
+                        applyDashboardTemplate(kibanaHost, configFile);
+                        setIndexRefreshInterval(esHost, DEFAULT_REFRESH_TIME);
+                    });
                 } catch (Exception e) {
                     e.printStackTrace(System.err);
                 }
@@ -40,7 +45,7 @@ public class ConfigureDashboards {
         }.start();
     }
 
-    private static void applyTemplate(String kibanaHost, InputStream configFile) {
+    private static void applyDashboardTemplate(String kibanaHost, InputStream configFile) {
         log.info("Checking Kibana setup...");
         HttpResponse<JsonNode> response = Unirest.get(String.format("http://%s:5601/api/saved_objects/_find", kibanaHost))
                 .queryString("type", "index-pattern")
@@ -70,6 +75,42 @@ public class ConfigureDashboards {
         } else {
             log.info("Kibana is already configured");
         }
+    }
+
+    private static void setIndexRefreshInterval(String elasticSearchHost, int seconds) {
+        log.info("Setting indices' refresh interval");
+        var response = Unirest.put(String.format("http://%s:9200/metricbeat-7.10.2/_settings", elasticSearchHost))
+                .header("Content-Type", "application/json")
+                .basicAuth("elastic", "changeme")   // ugh!
+                .body(String.format("{\"index\": { \"refresh_interval\": \"%ds\" } }", seconds))
+                .asJson();
+        if (response.getStatus() != 200)
+            throw new DashboardConfigurationError(String.format("Error %d changing indices' refresh interval for ES on host '%s'", response.getStatus(), elasticSearchHost));
+
+        log.info("Indices configured, now setting template for default refresh interval...");
+        HttpResponse<JsonNode> currentTemplate = Unirest.get(String.format("http://%s:9200/_template/metricbeat-7.10.2", elasticSearchHost))
+                .header("Accept", "application/json")
+                .basicAuth("elastic", "changeme")
+                .asJson();
+        if (currentTemplate.getStatus() != 200) {
+            log.warn("ES's not reachable yet.");
+            throw new DashboardConfigurationError(String.format("Failure %d contacting ES on host '%s'", response.getStatus(), elasticSearchHost));
+        }
+
+        var templatePayload = currentTemplate.getBody().getObject().getJSONObject("metricbeat-7.10.2");
+        templatePayload
+                .getJSONObject("settings")
+                .getJSONObject("index")
+                .put("refresh_interval", String.format("%ds", seconds));
+        response = Unirest.put(String.format("http://%s:9200/_template/metricbeat-7.10.2", elasticSearchHost))
+                .header("Content-Type", "application/json")
+                .basicAuth("elastic", "changeme")   // ugh!
+                .body(templatePayload)
+                .asJson();
+        if (response.getStatus() != 200)
+            throw new DashboardConfigurationError(String.format("Error %d changing indices template's refresh interval for ES on host '%s'", response.getStatus(), elasticSearchHost));
+
+        log.info("ElasticSearch indices are configured");
     }
 
     private static InputStream getLocalDashboardConfiguration() throws FileNotFoundException {
